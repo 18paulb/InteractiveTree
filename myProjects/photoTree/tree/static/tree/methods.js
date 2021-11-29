da60c5de993bed975d908f6e99007afe2542e5ca
Brandon Paul <bpaul18@byu.edu>
2021-11-29 11:18:28 -0700

    Worked on dragging methods

--- myProjects/photoTree/tree/static/tree/methods.js
+++ myProjects/photoTree/tree/static/tree/methods.js
@@ -1,27 +1,13 @@
-
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-  
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-  
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-}
-
 //Getting X,Y Positions Relative to current view of page
 function getPosition(id) {
     
     let tmpEl = document.getElementById(id);
-    let pos = tmpEl.getBoundingClientRect();
+    let pos = tmpEl.getBoundingClientRect();    //Gets position of element relative to viewport
 
     console.log(" Position of " + id + " Relative to Page View: \n")
     console.log("Top: " + pos.top + " Right: " + pos.right + " Bottom: " + pos.bottom + " Left: " + pos.left)
 
+    //This should get position relative to pageView
     /*
     let bodyRect = document.body.getBoundingClientRect()
     topOffset = pos.top - bodyRect.top
@@ -37,45 +23,29 @@ function getPosition(id) {
     
 }
 
-function changeSVG(id1, id2) {
+function changeSVG(id1, id2, SVGId) {
 
-    //FIXME I'm not getting the middle position, coordinates are wrong
+    //FIXME Coordinates are right SVG is being dumb and not showing, I think part of problem is positioning
     //Try doing it based on position relative to whole page
 
-
+//Get positions of the two elements
     let pos1 = getPosition(id1)
     let pos2 = getPosition(id2)
 
-    document.getElementById('SVG1').style.width = pos1.right - pos1.left
-    document.getElementById('SVG1').style.height = (pos1.bottom - pos1.top) + (pos2.bottom - pos2.top)
+//Tries to aproximate height and width of SVG    //FIXME Might need work
+    let width = Math.abs(pos1.left - pos2.right)
+    let height = Math.abs((pos1.bottom - pos1.top) + (pos2.bottom - pos2.top))
 
+    //Gets avg to get middle of div element
+    let x1 = (pos1.right + pos1.left) / 2
+    let y1 = (pos1.top + pos1.bottom) / 2
 
-    pos1 = getPosition(id1)
-    pos2 = getPosition(id2)
+    //Gets avg to get middle of div element
+    let x2 = (pos2.right + pos2.left) / 2
+    let y2 = (pos2.top + pos2.bottom) / 2
 
-    let x1 = (pos1.right / 2) + pos1.left
-    let y1 = pos1.top
+    document.getElementById(SVGId).innerHTML = '<svg width="' + width + '" height="' + height + '"><line x1=' + x1 + ' y1=' + y1 + ' x2=' + x2 + ' y2=' + y2 + ' stroke="red"/></svg>'
 
-    let x2 = (pos2.right / 2) + pos2.left
-    let y2 = pos2.top
-
-    document.getElementById('SVG1').innerHTML = '<line x1=' + x1 + ' y1=' + y1 + ' x2=' + x2 + ' y2=' + y2 + ' stroke="red"/>'
-
-
-/*
-var line = document.getElementById('SVG1')
-var div1 = document.getElementById('test1');
-var div2 = document.getElementById('test2');
-
-var x1 = div1.offsetLeft + (div1.style.width/2);
-var y1 = div1.offsetTop + (div1.style.height/2);
-var x2 = div2.offsetLeft + (div2.style.width/2);
-var y2 = div2.offsetTop + (div2.style.height/2);
-
-line.innerHTML = '<line x1=' + x1 + ' y1=' + y1 + ' x2=' + x2 + ' y2=' + y2 + ' stroke="red"/>'
-//line.attr('x1',x1).attr('y1',y1).attr('x2',x2).attr('y2',y2);
-*/
-    
 }
 
 
@@ -88,3 +58,7 @@ line.innerHTML = '<line x1=' + x1 + ' y1=' + y1 + ' x2=' + x2 + ' y2=' + y2 + ' 
 
 //Finding positions of elements https://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element
 //Use this to use a function that calculates position so that SVG line can follow wherever you drag too
+
+
+
+//For sorting in specific positions: https://jqueryui.com/sortable/
