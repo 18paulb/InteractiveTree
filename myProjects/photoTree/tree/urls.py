b736c863c855b2058d49cf91810035a21333231d
Brandon Paul <bpaul18@byu.edu>
2021-11-29 11:17:29 -0700

    Added Dragging Page

--- myProjects/photoTree/tree/urls.py
+++ myProjects/photoTree/tree/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+#from . views import DraggablePageView
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('draggable/', views.draggable, name='draggable'),
+    #path('draggable/', DraggablePageView.as_view(), name='draggable'),
 ]
--- myProjects/photoTree/tree/views.py
+++ myProjects/photoTree/tree/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#from django.views.generic import TemplateView
 
 # Create your views here.
 
 def index(request):
     return render(request, 'tree/index.html')
+
+
+def draggable(request):
+    return render(request, 'tree/draggable.html')
+
+#class DraggablePageView(TemplateView):
+#    template_name = 'draggable.html'
