973906fe86d792077945ec8129963bba5c1fb2ec
Brandon Paul <bpaul18@byu.edu>
2021-12-03 11:35:24 -0700

    Added Json of Data

---
+++ myProjects/photoTree/tree/static/tree/JS/parse.js
@@ -0,0 +1,123 @@
+
+let data = [
+  {
+    "image": 1,
+    "mother": 9,
+    "spouse": null,
+    "birthyear": 2006
+  },
+  {
+    "image": 2,
+    "mother": 9,
+    "spouse": null,
+    "birthyear": 2004
+  },
+  {
+    "image": 3,
+    "mother": 9,
+    "spouse": null,
+    "birthyear": 2002
+  },
+  {
+    "image": 4,
+    "mother": 9,
+    "spouse": null,
+    "birthyear": 2010
+  },
+  {
+    "image": 5,
+    "mother": 9,
+    "spouse": null,
+    "birthyear": 2008
+  },
+  {
+    "image": 6,
+    "mother": 9,
+    "spouse": null,
+    "birthyear": 2013
+  },
+  {
+    "image": 7,
+    "mother": 9,
+    "spouse": null,
+    "birthyear": 2001
+  },
+  {
+    "image": 8,
+    "mother": 11,
+    "spouse": 9,
+    "birthyear": 1978
+  },
+  {
+    "image": 9,
+    "mother": 10,
+    "spouse": 8,
+    "birthyear": 1979
+  },
+  {
+    "image": 10,
+    "mother": null,
+    "spouse": null,
+    "birthyear": 1950
+  },
+  {
+    "image": 11,
+    "mother": null,
+    "spouse": 12,
+    "birthyear": 1955
+  },
+  {
+    "image": 12,
+    "mother": null,
+    "spouse": 11,
+    "birthyear": 1955
+  },
+  {
+    "image": 13,
+    "mother": 11,
+    "spouse": 14,
+    "birthyear": 1980
+  },
+  {
+    "image": 14,
+    "mother": null,
+    "spouse": 13,
+    "birthyear": 1979
+  },
+  {
+    "image": 15,
+    "mother": 11,
+    "spouse": 17,
+    "birthyear": 1984
+  },
+  {
+    "image": 16,
+    "mother": 15,
+    "spouse": null,
+    "birthyear": 2005
+  },
+  {
+    "image": 17,
+    "mother": null,
+    "spouse": 15,
+    "birthyear": 1981
+  }
+ ]
+
+
+for (let i = 0; i < data.length; ++i) {
+  let pic = data[i].image.toString()
+  let year = data[i].birthyear.toString()
+  document.getElementById('nodes').innerHTML += `<div class="node"><img class="photo" src="../../static/tree/images/pictures/${pic}.PNG"/>
+  <span style='margin-top:5px;'>${year}</span></div>`
+}
+
+
+//This only works if there is a data structure that has multiple keys
+//I need a data structure to have all the mother-child relationships, maybe a map with multiple values(children) to each key(mother)
+//Consider having key (mother) that connects to Object (Mother Class) that contains array of chilren
+
+
+
+
+
