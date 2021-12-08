21b27df1151ddf68a4f0cf718207410238775006
Brandon Paul <bpaul18@byu.edu>
2021-12-08 11:22:38 -0700

    Tried new dragging methods

--- myProjects/photoTree/tree/static/tree/JS/drag.js
+++ myProjects/photoTree/tree/static/tree/JS/drag.js
@@ -1,4 +1,5 @@
 //Drag and drop section
+/*
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -12,17 +13,38 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
-
+*/
 
 //To allow dropping between elements, consider messing around with DOM
 
 
 
+//Test code, will probably delete
+function drag_start(event) {
+    var style = window.getComputedStyle(event.target, null);
+    event.dataTransfer.setData("text/plain",
+       (parseInt(style.getPropertyValue("left"), 10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"), 10) - event.clientY));
+ }
 
 
 
+ function drop(event) {
+    var offset = event.dataTransfer.getData("text/plain").split(',');
+    var dm = document.getElementById('dragme');
+    dm.style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
+    dm.style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
+    event.preventDefault();
+    return false;
+ }
+ function drag_over(event) {
+    event.preventDefault();
+    return false;
+ }
 
-
+var dm = document.getElementById('dragme');
+dm.addEventListener('dragstart', drag_start, false);
+document.body.addEventListener('dragover', drag_over, false);
+document.body.addEventListener('drop', drop, false);
 
 
 
