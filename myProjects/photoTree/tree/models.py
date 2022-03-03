e73159d59da15eccc93a8f2cb06319fbdfa4787c
Brandon <bjpaul99@gmail.com>
2022-03-03 10:58:30 -0700

    Made model for Nodes

--- myProjects/photoTree/tree/models.py
+++ myProjects/photoTree/tree/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Node(models.Model):
+  
+  image = models.IntegerField(default=None);
+  mother = models.IntegerField(default=None);
+  spouse = models.IntegerField(default=None);
+  birthyear = models.IntegerField(default=None);
+
+  def __str__(self):
+    return str(self.image)
