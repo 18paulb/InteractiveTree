728a5353f353816eb38eaafe0ff5c4f3da079178
Brandon Paul <bpaul18@byu.edu>
2022-03-14 12:49:55 -0600

    Reading from backend

--- myProjects/photoTree/tree/urls.py
+++ myProjects/photoTree/tree/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-
 from . import views
+from tree.views import (
+    getNodes,
+)
 
 #from . views import DraggablePageView
 
@@ -9,5 +11,8 @@ urlpatterns = [
     path('refactor/', views.refactor, name='refactor'),
     path('tree/', views.tree, name='tree'),
     path('chart/', views.chart, name='tree'),
-    path('delete/', views.delete, name='delete')
+    path('delete/', views.delete, name='delete'),
+    
+    #For GET
+    path('get/ajax/getNodes', getNodes, name = 'getNodes'),
 ]
