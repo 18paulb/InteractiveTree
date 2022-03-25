81c24ec024240682d66494ab1fa8bd3762d1847d
Carter Adams <cladams7905@gmail.com>
2022-03-25 15:14:02 -0600

    Fixed some problems with removal of the spouse lines

--- myProjects/photoTree/tree/static/tree/JS/chart.js
+++ myProjects/photoTree/tree/static/tree/JS/chart.js
@@ -759,7 +759,7 @@ function removeSpouseLine(id1, id2) {
     let elem2X = svgLines[i].x2.baseVal.value;
     let elem2Y = svgLines[i].y2.baseVal.value;
 
-    if (node2XPos == elem1X && node1XPos == elem2X && elem1Y == elem2Y) {
+    if (node2XPos == elem1X && node1XPos == elem2X && (node1YPos - elem1Y) < 30 && (node2YPos - elem2Y) < 30) {
       svgLines[i].remove();
     }
   }
@@ -1018,7 +1018,7 @@ function hoverMenu(nodeId) {
   //Make this class a datapoint technically and make XY pos's from there, just get X,Y from node and then adjust slightly for it to be near node
   hMenu.innerHTML = `
   <div id='hover-menu' class='hover-menu hover-point' style='--y: ${nodeY + 100}px; --x: ${nodeX - 25}px'>
-    <div>Gen: ${getGeneration(data[nodeIndex])} <br> Node: ${data[nodeIndex].image}</b><br>x: ${nodeX}</div>
+    <div>Gen: ${getGeneration(data[nodeIndex])} <br> Node: ${data[nodeIndex].image}</b><br>Spouse: ${data[nodeIndex].spouse}<br>x: ${nodeX}</div>
       <img class='menu-pic' src='../../static/tree/images/pictures/${nodeId}.PNG'/>
       <div id ='node-${nodeId}-info' style='display: flex; justify-content:center; align-items:center; flex-direction: column;'>
         <div><b>John Doe</br></div>
